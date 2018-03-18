78bb9bd67c14d311009a7be49678b75d063c0a9c
wesol <mwesolowski88@gmail.com>
2018-03-18 19:57:29 +0100

    Basic structure done

---
+++ src/main/java/com/meeting/MeetingPortalApplication.java
@@ -0,0 +1,12 @@
+package com.meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingPortalApplication.class, args);
+	}
+}
